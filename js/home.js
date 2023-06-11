cd2834df98cbf06a281a5fe08a5c9c90cda5a4a5
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-06-11 01:13:29 -0400

    button labels :

--- js/home.js
+++ js/home.js
@@ -1,10 +1,10 @@
 window.onload = function(){
     if(localStorage.getItem("username") != null){
         document.querySelector(".container").innerHTML = `<div class = "logItem" id="profile" title="View Profile">
-        <span class="logText"></span><i class="fa-solid fa-user"></i></span>
+        <span class="logText"></span><i class="fa-solid fa-user"></i></span><div class="signtext">Profile</div>
     </div>
     <div class="logItem" id="logout" title="Log Out">
-        <span class="logText"></span><i class="fa-solid fa-right-from-bracket fa-flip-horizontal"></i></span>
+        <span class="logText"></span><i class="fa-solid fa-right-from-bracket fa-flip-horizontal"></i></span><div class="signtext1">Log Out</div>
     </div>`;
     document.getElementById("logout").addEventListener(("click"),()=>{
         window.location.href="../html/logout.html";
@@ -14,10 +14,10 @@ window.onload = function(){
     })
     }else{
         document.querySelector(".container").innerHTML = `<div class = "logItem" id="signup" title="Sign Up">
-        <span class="logText"></span><i class="fa-solid fa-user-plus" ></i></span>
+        <span class="logText"></span><i class="fa-solid fa-user-plus" ></i></span><div class="signtext2">Sign Up</div>
     </div>
     <div class="logItem" id="login" title="Log In">
-        <span class="logText"></span><i class="fa-solid fa-right-to-bracket"></i></span>
+        <span class="logText"></span><i class="fa-solid fa-right-to-bracket"></i></span><div class="signtext3">Log In</div>
     </div>`;
     document.getElementById("signup").addEventListener(("click"),()=>{
         window.location.href="../html/signup.html";
