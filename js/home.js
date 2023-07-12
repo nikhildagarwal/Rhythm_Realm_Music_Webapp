e2e0ec634d4c704aa1c8749677ad2296c68d33af
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-07-12 10:03:04 -0400

    ui changes

--- js/home.js
+++ js/home.js
@@ -93,11 +93,11 @@ window.onload = function(){
                         tabContainer[2].className = "listen_tab";
                     }
                 })
-                    document.querySelector(".container").innerHTML = `<div class = "logItem1" id="profile" title="View Profile">
-                    <span class="logText"></span><i class="fa-solid fa-user"></i></span><div class="signtext">Profile</div>
+                    document.querySelector(".container").innerHTML += `<div class = "logItem1" id="profile" title="View Profile">
+                    <span class="logText"><i class="fa-solid fa-user"></i><div class="signtext">Profile</div>
                 </div>
                 <div class="logItem" id="logout" title="Log Out">
-                    <span class="logText"></span><i class="fa-solid fa-right-from-bracket fa-flip-horizontal"></i></span><div class="signtext1">Log Out</div>
+                    <span class="logText"><i class="fa-solid fa-right-from-bracket fa-flip-horizontal"></i><div class="signtext">Log Out</div>
                 </div>`;
                 document.getElementById("logout").addEventListener(("click"),()=>{
                     window.location.href="/api/logout/";
@@ -212,11 +212,11 @@ window.onload = function(){
         tabArray[2].addEventListener('click',()=>{
             alert("You must (Log In) / (Create An Account) before you can access LISTEN NOW");
         })
-                    document.querySelector(".container").innerHTML = `<div class = "logItem" id="signup" title="Sign Up">
-                    <span class="logText"></span><i class="fa-solid fa-user-plus" ></i></span><div class="signtext2">Sign Up</div>
+                    document.querySelector(".container").innerHTML += `<div class = "logItem" id="signup" title="Sign Up">
+                    <span class="logText"><i class="fa-solid fa-user-plus" ></i><div class="signtext">Sign Up</div>
                 </div>
                 <div class="logItem" id="login" title="Log In">
-                    <span class="logText"></span><i class="fa-solid fa-right-to-bracket"></i></span><div class="signtext3">Log In</div>
+                    <span class="logText"><i class="fa-solid fa-right-to-bracket"></i><div class="signtext">Log In</div>
                 </div>`;
                 document.getElementById("signup").addEventListener(("click"),()=>{
                     window.location.href="/api/signup";
