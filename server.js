cc1add97b75532f976cb1e5a15d725a54fdeecb9
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-07-25 00:26:26 -0400

    remove temp server

--- server.js
+++ server.js
@@ -234,9 +234,9 @@ const server = http.createServer((req,res) => {
     });
 });
 
-server.listen(3000, "localhost", () => {
+/*server.listen(3000, "localhost", () => {
     console.log("Listening on port 3000");
-});
+});*/
 
 async function handleChangedPasswordNotif(req,res,email){
     try{
